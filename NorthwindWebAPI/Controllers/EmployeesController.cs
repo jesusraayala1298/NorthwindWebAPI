0bbce4fb7847830f102071d46730950e07ff0cf7
Jesus Ramirez Ayala <48115771+jesusraayala1298@users.noreply.github.com>
2022-05-22 22:58:43 -0500

    Agregar archivos de proyecto.

---
+++ NorthwindWebAPI/Controllers/EmployeesController.cs
@@ -0,0 +1,219 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using NorthwindWebAPI.Data;
+using NorthwindWebAPI.Models;
+
+namespace NorthwindWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeesController : ControllerBase
+    {
+        private readonly NorthwindContext _context;
+
+        public EmployeesController(NorthwindContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Employees
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Employee>>> GetEmployees()
+        {
+            return await _context.Employees.ToListAsync();
+        }
+
+        // GET: api/Products
+        [HttpGet]
+        [Route("Products")]
+        public async Task<ActionResult<IEnumerable<Product>>> AllProducts()
+        {
+            return await _context.Products.OrderBy(e=>e.ProductName).ToListAsync();
+        }
+
+        // GET: api/
+        [HttpGet]
+        [Route("ByCompany")]
+        public IEnumerable<Object> GetEmployeesByCompany()
+        {
+            return _context.Employees
+                .GroupBy(e => e.CompanyId)
+                .Select(e => new {
+                    Company = e.Key,
+                    Empleados = e.Count()
+                });
+            
+        }
+
+        // GET: api/
+        [HttpGet]
+        [Route("top5")]
+        public IEnumerable<Object> GetTop5()
+        {
+            return _context.Employees
+                .Where(e =>  e.CompanyId == 1)
+                .Join(_context.Movements,
+                e => e.EmployeeId,
+                m => m.EmployeeId,
+                (e, m) => new
+                {
+                    Empleado = e.FirstName + " " + e.LastName,
+                    IdMovimiento = m.MovementId,
+                    Anio = m.Date.Year
+                })
+                .Where(em=>em.Anio==1996)
+                .Join(_context.Movementdetails,
+                em => em.IdMovimiento,
+                md => md.MovementId,
+                (em, md) => new
+                {
+                    Empleado = em.Empleado,
+                    Cantidad = md.Quantity
+                })
+                .GroupBy(e=> e.Empleado)
+                .Select(e=> new { 
+                    Empleado = e.Key,
+                    Ventas = e.Sum(g=>g.Cantidad)
+                })
+                .OrderByDescending(e => e.Ventas)
+                .Take(5);
+
+        }
+
+        // GET: api/
+        [HttpGet]
+        [Route("Productos_mes")]
+        public IEnumerable<Object> GetProducts(int anio, string idProducto)
+        {
+            return _context.Products
+                .Where(e => e.CompanyId == 1)
+                .Join(_context.Movementdetails,
+                e => e.ProductId,
+                m => m.ProductId,
+                (e, m) => new
+                {
+                    Producto = e.ProductId,
+                    Nombre = e.ProductName,
+                    IdMovimiento = m.MovementId,
+                    Cantidad = m.Quantity
+                })
+                .Join(_context.Movements,
+                em => em.IdMovimiento,
+                md => md.MovementId,
+                (em, md) => new
+                {
+                    Producto = em.Producto,
+                    Nombre = em.Nombre,
+                    Cantidad = em.Cantidad,
+                    Anio = md.Date.Year,
+                    Mes = md.Date.Month,
+                    Fecha=md.Date
+                })
+                .Where(e => e.Anio==anio && e.Nombre.Equals(idProducto))
+                .GroupBy(e => e.Mes)
+                .Select(e => new{
+                    Mes = e.Key,
+                    Ventas = e.Sum(g=>g.Cantidad)
+                })
+                .OrderBy(e => e.Mes);
+        }
+
+        
+
+        // GET: api/Employees/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Employee>> GetEmployee(int id)
+        {
+            var employee = await _context.Employees.FindAsync(id);
+
+            if (employee == null)
+            {
+                return NotFound();
+            }
+
+            return employee;
+        }
+
+        // GET: api/Employees/5
+        [HttpGet("GetProduct")]
+        public async Task<ActionResult<Product>> GetProduct(int id)
+        {
+            var product = await _context.Products.FindAsync(id);
+
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return product;
+        }
+
+        // PUT: api/Employees/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutEmployee(int id, Employee employee)
+        {
+            if (id != employee.EmployeeId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(employee).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!EmployeeExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Employees
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Employee>> PostEmployee(Employee employee)
+        {
+            _context.Employees.Add(employee);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetEmployee", new { id = employee.EmployeeId }, employee);
+        }
+
+        // DELETE: api/Employees/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteEmployee(int id)
+        {
+            var employee = await _context.Employees.FindAsync(id);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+
+            _context.Employees.Remove(employee);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool EmployeeExists(int id)
+        {
+            return _context.Employees.Any(e => e.EmployeeId == id);
+        }
+    }
+}
